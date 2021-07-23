a3fafde3affa13e3c15aac772e0f4e5aebab3213
Ronan Venkat <47954337+DanorRon@users.noreply.github.com>
2021-07-23 19:06:00 -0400

    Add inner classes and documentation for class and main method

--- src/MonotoneTriangulatorHoles.java
+++ src/MonotoneTriangulatorHoles.java
@@ -1,2 +1,15 @@
-public class MonotoneTriangulatorHoles {
+/**
+ * Uses monotone triangulation to split a polygon, possibly with holes, into triangles
+ * @author Ronan Venkat
+ * @version 7/23/2021
+ */
+public class MonotoneTriangulatorHoles
+{
+    /**
+     * Main method
+     */
+
+    private class Vert
+
+    private class Edge
 }
